3cf173b0501390eec1d90b6efe033cd574624ce0
Jordan Toering <toering3@gmail.com>
2019-02-05 10:31:47 -0800

    parameterize username for shell commands

--- runScraper.sh
+++ runScraper.sh
@@ -1,10 +1,13 @@
 #!/bin bash
 
+# Set username here
+username=jtoering
+
 # Scrape the events
 python3 eventScraper.py
 
 # Upload to ftp server
 echo "Uploading to ftp server, please enter password"
-scp eventBriteEvents.json jtoering@fraser.sfu.ca:/home/jtoering/pub_html/eventbrite
+scp eventBriteEvents.json $username@fraser.sfu.ca:/home/$username/pub_html/eventbrite
 echo "Changing permissions of eventbrite file, please enter password"
-ssh jtoering@fraser.sfu.ca chmod 644 pub_html/eventbrite/eventBriteEvents.json
+ssh $username@fraser.sfu.ca chmod 644 pub_html/eventbrite/eventBriteEvents.json
