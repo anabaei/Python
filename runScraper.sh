f7da217b9660451f10c1293b383b3e6ac77b4d43
Amir Nabaei <nabaei17@gmail.com>
2019-04-08 12:22:26 -0700

    reading from file and not endpoint

--- runScraper.sh
+++ runScraper.sh
@@ -1,17 +1,18 @@
 #!/bin bash
 
 # Set username here
-username=anabaei
+#username=anabaei
 
 # Set ftp directory here
-directory=pub_html/eventbrite/
+#directory=pub_html/eventbrite/
 
 # Scrape the events
 python3 eventScraper.py
 
 # Upload to ftp server
-echo "Uploading to ftp server, please enter password"
-scp eventBriteEvents.json $username@fraser.sfu.ca:/home/${username}/${directory}
-echo "Changing permissions of eventbrite file, please enter password"
-ssh $username@fraser.sfu.ca chmod 644 ${directory}eventBriteEvents.json
+#echo "Uploading to ftp server, please enter password"
+#scp eventBriteEvents.json $username@fraser.sfu.ca:/home/${username}/${directory}
+#echo "Changing permissions of eventbrite file, please enter password"
+#ssh $username@fraser.sfu.ca chmod 644 ${directory}eventBriteEvents.json
+cp eventBriteEvents.json  /Users/amirnabaei/Desktop/sites/server_salesforce_scrapper/server_auth
 echo "Finished. Combined json file should be in https://sfu.ca/~${username}/eventbrite/eventBriteEvents.json"
