82373772bd16c0d41c993669585a95653ebe71b6
amirnabaei <87382930+amirnabaei@users.noreply.github.com>
2022-01-25 09:05:56 -0800

    update changes

---
+++ sample/sanbox.py
@@ -0,0 +1,29 @@
+class SLLNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+     # this method returns a readable anyobjest we have. It returns as string format our self.data
+    def __repr__(self):
+        return "SLLNode object: data={}".format(self.data)
+
+    def get_data(self):
+        """Return the self.data attribute."""
+        return self.data
+
+    def set_data(self, new_data):
+        """Replace the existing value of the self.data attribute with new_data
+        parameter."""
+        self.data = new_data
+
+    def get_next(self):  
+        """Return the self.next attribute"""
+        return self.next
+
+    def set_next(self, new_next):
+        """Replace the existing value of the self.next attribute with new_next"""
+        self.next = new_next        
+
+
+
+
